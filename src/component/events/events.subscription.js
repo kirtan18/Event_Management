b7710b25fe5eee161d24b77cd5225c7cb1196fab
kirtan18 <kirtanprajapati6602@gmail.com>
2023-05-30 13:00:03 +0530

    update event Dal query

--- src/component/events/events.subscription.js
+++ src/component/events/events.subscription.js
@@ -1,3 +1,69 @@
+// const { withFilter } = require('graphql-subscriptions');
+// const PubSub = require('PubSub');
+// const { EventType } = require('./events.types');
+
+// const pubsub = new PubSub();
+
+// module.exports = {
+//   eventUpdated: {
+//     type: EventType,
+//     subscribe: withFilter(
+//       () => pubsub.asyncIterator('EVENT_UPDATED'),
+//       (payload, variables) => payload.eventId === variables.eventId
+//     ),
+//     resolve: (payload) => payload,
+//   },
+//   eventCreated: {
+//     type: EventType,
+//     subscribe: () => pubsub.asyncIterator('EVENT_CREATED'),
+//   },
+//   resolve: (payload) => payload,
+// };
+
+// function publishEventUpdated(event) {
+//   pubsub.publish('EVENT_UPDATED', { eventUpdated: event });
+// }
+
+// function publishEventCreated(event) {
+//   pubsub.publish('EVENT_CREATED', { eventCreated: event });
+// }
+
+// module.exports.publishEventUpdated = publishEventUpdated;
+// module.exports.publishEventCreated = publishEventCreated;
+// const { withFilter } = require('graphql-subscriptions');
+// const PubSub = require('PubSub');
+// const { EventType } = require('./events.types');
+
+// const pubsub = new PubSub();
+
+// module.exports = {
+//   eventUpdated: {
+//     type: EventType,
+//     subscribe: withFilter(
+//       () => pubsub.asyncIterator('EVENT_UPDATED'),
+//       (payload, variables) => payload.eventId === variables.eventId
+//     ),
+//     resolve: (payload) => payload,
+//   },
+//   eventCreated: {
+//     type: EventType,
+//     subscribe: withFilter(
+//       () => pubsub.asyncIterator('EVENT_CREATED'),
+//     ),
+//     resolve: (payload) => payload,
+//   },
+// };
+
+// function publishEventUpdated(event) {
+//   pubsub.publish('EVENT_UPDATED', { eventUpdated: event });
+// }
+
+// function publishEventCreated(event) {
+//   pubsub.publish('EVENT_CREATED', { eventCreated: event });
+// }
+
+// module.exports.publishEventUpdated = publishEventUpdated;
+// module.exports.publishEventCreated = publishEventCreated;
 const { withFilter } = require('graphql-subscriptions');
 const PubSub = require('PubSub');
 const { EventType } = require('./events.types');
@@ -15,9 +81,10 @@ module.exports = {
   },
   eventCreated: {
     type: EventType,
-    subscribe: () => pubsub.asyncIterator('EVENT_CREATED'),
+    subscribe: withFilter(
+      () => pubsub.asyncIterator('EVENT_CREATED')
+    )
   },
-  resolve: (payload) => payload,
 };
 
 function publishEventUpdated(event) {
